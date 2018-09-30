980177969fd85c43e34d1acc6a676edf1344d3a7
knoxg <knoxg@192.168.0.88>
2018-10-01 09:57:16 +1000

    timer classes

---
+++ src/main/java/com/randomnoun/common/timer/Benchmark.java
@@ -0,0 +1,365 @@
+package com.randomnoun.common.timer;
+
+/* (c) 2013 randomnoun. All Rights Reserved. This work is licensed under a
+ * BSD Simplified License. (http://www.randomnoun.com/bsd-simplified.html)
+ */
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+import com.randomnoun.common.Text;
+
+/**
+ * An object that performs time benchmarking.
+ *
+ * <p>This object contains a reference to all benchmark instances
+ * that are currently executing in the VM, referenced by <code>benchId</code>.
+ * It is encouraged that each individual request has a unique benchId, although
+ * this is not required unless you need to use the {@link #getBenchmark} method.
+ *
+ * (if multiple instances are created using the same <code>benchId</code>,
+ * only the most recent instance will be retrievable via getBenchmark()).
+ *
+ * <p>Multiple checkpoints can be set during the course of a benchmark, which
+ * will be written to disk after completion.
+ *
+ * <p>Output is generated only after the benchmark has completed. All output is
+ * buffered, and references to any open benchmark files are kept within this
+ * object in order to minimise the amount of overhead that benchmarking will
+ * impose.
+ *
+ * <p>This class does not expire benchmarks, and therefore may cause memory leaks
+ * if benchmarks are not closed correctly.
+ *
+ * <p>Output is of the form:
+ * <pre style="code">
+ *   benchId1,begin,timestamp,pointID1,timestamp1,pointID1,timestamp2,[...],end,timestampn,duration
+ *   benchId2,begin,timestamp,pointID1,timestamp1,pointID1,timestamp2,[...],end,timestampn,duration
+ *   :
+ *   :
+ * </pre>
+ *
+ * <p>where <i>benchId</i> is the benchId indentifying this benchmark, and
+ * <i>pointIDn</i> are the individual checkpoint identifiers. Each line finishes
+ * with the text ",duration," followed by the duration of the entire benchmark, in
+ * milliseconds. Timestamps are displayed in the format defined by the
+ * {@link #setDateFormat(String)} method.
+ *
+ * <p>There is no current way of following a benchmark through the EJB boundary layer;
+ * to do so would currently involve overriding the User object, which I'm not terribly keen to do.
+ * It is possible to set up a new Benchmark on the EJB side, and track performance
+ * independently over there. It should then be a simple matter to heuristically match up
+ * individual HTTP requests/struts actions and the EJB methods that they invoke
+ * (under normal load conditions). It's important, however, that two Benchmark engines in the
+ * same VM do not write to the same file, as they will corrupt each other's output.
+ *
+ * @author knoxg
+ * 
+ */
+public class Benchmark {
+    /** A string to use in stack traces. */
+    public static String _revision = "$Id$";
+
+
+    /** A Comparator which performs comparisons between two Benchmarks.
+     */
+    public static class BenchmarkComparator implements Comparator<Benchmark> {        
+
+    	/** Create a new BenchmarkComparator object */
+        public BenchmarkComparator() {
+        }
+
+        /** Compare two structured list elements
+         *
+         * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+         */
+        public int compare(Benchmark a, Benchmark b) throws IllegalArgumentException {
+            Benchmark benchmarkA = (Benchmark) a;
+            Benchmark benchmarkB = (Benchmark) b;
+
+            return benchmarkA.startTime < benchmarkB.startTime ? -1 : 
+              (benchmarkA.startTime > benchmarkB.startTime ? 1 : 0);
+        }
+    }
+
+    /** A class containing the checkpoints for a single benchmark */
+    public class CheckpointList {
+        /** List of Checkpoints, containing the checkpoints for a single
+         *  benchmark */
+        List<Checkpoint> list = new ArrayList<Checkpoint>();
+
+        /** Return an iterator for the checkpoint list */
+        public Iterator<Checkpoint> iterator() {
+            return list.iterator();
+        }
+
+        /** Retrieve an individual checkpoint */
+        public Checkpoint get(int n) {
+            return (Checkpoint) list.get(n);
+        }
+
+        /** Adds a checkpoint to this set. The checkpoint is stamped using
+         *  the current system time. */
+        public void addCheckPoint(String id) {
+            list.add(new Checkpoint(id));
+        }
+
+        /** Adds an annotation to this set. */
+        public void addAnnotation(String annotation) {
+            list.add(new Checkpoint(annotation, -1));
+        }
+        
+        
+        /** Returns a string version of this set. Timestamps are returned
+         *  using the format specified by the Benchmark class */
+        public String toString() {
+            int i;
+            int size = list.size();
+            StringBuffer sb = new StringBuffer();
+            Checkpoint checkpoint;
+            SimpleDateFormat dateFormat;
+
+            dateFormat = new SimpleDateFormat(Benchmark.dateFormatText);
+
+            for (i = 0; i < size; i++) {
+                checkpoint = (Checkpoint) list.get(i);
+                if (checkpoint.getTimestamp()==-1) {
+	                sb.append(',');
+	                sb.append(Text.escapeCsv("\u0087" + checkpoint.id)); // ASCII 135, Hex 87
+                } else {
+	                sb.append(',');
+	                sb.append(Text.escapeCsv(dateFormat.format(checkpoint.getDate())));
+	                sb.append(',');
+	                sb.append(Text.escapeCsv(checkpoint.id));
+                }
+            }
+
+            if (list.size() > 0) {
+                sb.append(",t,");
+                checkpoint = (Checkpoint) list.get(list.size() - 1);
+                sb.append(HiResTimer.getElapsedMillis(((Checkpoint) list.get(0)).timestamp, checkpoint.timestamp));
+            }
+
+            return sb.toString();
+        }
+    }
+
+    /** A single benchmark point. May be either a begin, end, or checkpoint.
+     * 
+     * A checkpoint with a timestamp of -1 is an annotation.
+     */
+    public class Checkpoint {
+        /** The string ID for this benchmark point */
+        String id;
+
+        /** The time at which this point was created to the benchmark */
+        long timestamp;
+
+        public Checkpoint(String id) {
+            this.id = id;
+            this.timestamp = HiResTimer.getTimestamp();
+        }
+
+        public Checkpoint(String id, long timestamp) {
+            this.id = id;
+            this.timestamp = timestamp;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public Date getDate() {
+            // return date;
+            return new Date(startTime + HiResTimer.getElapsedMillis(startHiresTimestamp, timestamp));
+        }
+    }
+
+    /** Hashtable mapping filenames to java.io.Writer objects */
+    private static Hashtable<String, Writer> writers = null;
+
+    /** Hashtable mapping benchmark IDs to Benchmark objects */
+    private static Hashtable<String, Benchmark> benchmarks = null;
+
+    /** date format for timestamps. The default date format is
+     *  the SimpleDateFormat <tt>"yyyy-dd-MM HH:mm:ss.SSS"</tt>. Note that
+     *  better-than-millisecond precision is unavailable using the
+     *  standard Java-supplied timer functions. */
+    private static String dateFormatText = null;
+
+    /** the set of checkpoints for this benchmark instance */
+    protected CheckpointList checkpointList = null;
+
+    /** the string ID for this benchmark */
+    private String benchId = null;
+
+    /** Start time, in milliseconds */
+    protected long startTime;
+
+    /** HiRes Timer token for start of benchmark */
+    protected long startHiresTimestamp;
+
+    /** True if benchmark is active, false if the benchmark has completed. */
+    protected boolean active;
+
+    /** the file where the results of this benchmark will be written */
+    private String filename = null;
+
+    /** Returns a PrintWriter that can be written to in order to
+     *  commit a benchmark to disk. */
+    private static PrintWriter open(String filename)
+        throws IOException {
+        PrintWriter out;
+
+        // use existing Writer if it is already open
+        if (writers.get(filename) != null) {
+            return (PrintWriter) writers.get(filename);
+        }
+
+        // 'true' flag indicates files will be appended to
+        out = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)));
+        writers.put(filename, out);
+
+        return out;
+    }
+
+    /** Instantiates a new benchmark. Automatically creates a 'begin' checkpoint
+     *  for this new instance. */
+    public Benchmark(String benchId, String filename) {
+        startTime = System.currentTimeMillis();
+        startHiresTimestamp = HiResTimer.getTimestamp();
+
+        checkpointList = new CheckpointList();
+        benchmarks.put(benchId, this);
+        checkpointList.addCheckPoint("begin");
+        this.benchId = benchId;
+        this.filename = filename;
+        this.active = true;
+    }
+
+    /** Alters the ID for a benchmark that is already running. */
+    public void setBenchId(String benchId) {
+        benchmarks.remove(this.benchId);
+        this.benchId = benchId;
+        benchmarks.put(benchId, this);
+    }
+
+    /** Performs checkpoint. */
+    public void checkpoint(String pointID) {
+        checkpointList.addCheckPoint(pointID);
+    }
+    
+    /** Creates a benchmark annotation */
+    public void annotate(String annotation) {
+        checkpointList.addAnnotation(annotation);
+    }
+
+
+    /** Returns a string representation of this benchmark */
+    public String toString() {
+        return benchId + checkpointList.toString();
+    }
+
+    /** Completes benchmark and writes to disk */
+    public void end()
+        throws IOException {
+        PrintWriter printWriter;
+
+        checkpointList.addCheckPoint("end");
+        if (filename != null) {
+            printWriter = (PrintWriter) writers.get(filename);
+            if (printWriter == null) {
+                printWriter = open(filename);
+            }
+            printWriter.println(this.toString());
+            printWriter.flush();
+        }
+        active = false;
+        benchmarks.remove(benchId);
+    }
+
+    /** Returns whether this benchmark is currently active */
+    public boolean isActive() {
+        return active;
+    }
+
+
+    /** Cancel this benchmark */
+    public void cancel() {
+        benchmarks.remove(benchId);
+    }
+
+    /** Returns all checkpoints stored in this benchmark */
+    public CheckpointList getCheckpointList() {
+        return checkpointList;
+    }
+
+    /** Returns the benchmark ID */
+    public String getId() {
+        return benchId;
+    }
+
+    /** Returns the benchmark with the supplied ID */
+    public static Benchmark getBenchmark(String benchId) {
+        return (Benchmark) benchmarks.get(benchId);
+    }
+
+    /** Sets the date format for all output of this class. The date format
+     * is represented as a string, rather than a DateFormat object, since
+     * most SimpleDateFormats are not thread-safe. */
+    public static void setDateFormat(String dateFormatText) {
+        Benchmark.dateFormatText = dateFormatText;
+    }
+
+    /** Flushes all writers within this benchmark object (commits any
+     *  unwritten data to disk). Files are still kept open. */
+    public static void flushWriters()
+        throws IOException {
+        Enumeration<Writer> e;
+
+        for (e = writers.elements(); e.hasMoreElements();) {
+            ((Writer) e.nextElement()).flush();
+        }
+    }
+
+    /** Closes all writers within this benchmark object. Files will be
+     *  automatically reopened for any running benchmarks.  */
+    public static void closeWriters()
+        throws IOException {
+        Enumeration<Writer> e;
+
+        for (e = writers.elements(); e.hasMoreElements();) {
+            ((Writer) e.nextElement()).close();
+        }
+
+        // remove references to old writers
+        writers = new Hashtable<String, Writer>();
+    }
+    
+    public String toJson() {
+    	String result = "[";
+		   Iterator<Checkpoint> i = getCheckpointList().iterator();
+		   // Benchmark.Checkpoint cpStart = i.hasNext() ? (Benchmark.Checkpoint) i.next() : null;
+		   while (i.hasNext()) {
+			   Benchmark.Checkpoint cpNext = (Benchmark.Checkpoint) i.next();
+			   result += "{\"id\":\"" + Text.escapeJavascript(cpNext.getId()) + "\",\"timestamp\":" + cpNext.getTimestamp() + "}";
+			   if (i.hasNext()) { result += ","; }
+		   }
+		   result += "]";
+		   return result;
+    }
+    
+
+    static {
+        writers = new Hashtable<String, Writer>();
+        benchmarks = new Hashtable<String, Benchmark>();
+
+        // default date format
+        dateFormatText = "yyyy-MM-dd HH:mm:ss.SSS";
+    }
+}
---
+++ src/main/java/com/randomnoun/common/timer/HiResTimer.java
@@ -0,0 +1,263 @@
+package com.randomnoun.common.timer;
+
+/* (c) 2013 randomnoun. All Rights Reserved. This work is licensed under a
+ * BSD Simplified License. (http://www.randomnoun.com/bsd-simplified.html)
+ */
+
+import org.apache.log4j.Logger;
+
+import com.randomnoun.common.timer.HiResTimer;
+
+/**
+ * This is some increasingly ancient code to provide microsecond timer resolution in Java 
+ * using JNI on Windows.
+ * 
+ * <p>Java now provides this natively via System.nanoTime(), so all of this can and should be thrown out
+ * some day. 
+ * 
+ * <p>Historical background: Java previously supplied only millisecond
+ * resolution, and on Windows this was only around 10-20 millisecond precision;
+ * approx 16ms (Win3.1), 55ms (ME/98/95) or 10ms (others)). Other platforms (Linux/Solaris)
+ * appear to provide real millisecond resolution.
+ *
+ * <p>This class uses JNI to access the Windows QueryPerformanceCounter API. It
+ * requires the HiResTimer.dll to be located in the VM's java.library.path. In order to
+ * work in WSAD, it will also examine the contents of the 'workspace.root' System
+ * property, and attempt to reference it from within the developer's build tree.
+ *
+ * <p>If the native library cannot be loaded, this class falls back to Java's
+ * standard timer implementation (System.currentTimeMillis).
+ *
+ * <p>Note that the Websphere reloading ClassLoader cannot deal with JNI,
+ * so this class must exist in an external classpath or JAR which is referenced from
+ * the system classpath. The Ant build task 'buildStartupJar' in ext/build.xml
+ * can compile the JNI and create this startup JAR.
+ *
+ * <hr/>
+ *
+ * <p>This file was based on the discussion at
+ * <br/><a href="http://www.fawcette.com/archives/premier/mgznarch/javapro/2001/08aug01/km0108/km0108-1.asp">
+ * http://www.fawcette.com/archives/premier/mgznarch/javapro/2001/08aug01/km0108/km0108-1.asp</a>
+ *
+ * <p>Some instructions on building JNI in a cygwin/gcc environment was found at
+ * <br/><a href="http://www.inonit.com/cygwin/jni/helloWorld/">
+ * http://www.inonit.com/cygwin/jni/helloWorld/</a>
+ * 
+ * <p>Documentation on Windows APIs can be found in Windows 2003 Core API download, found at
+ * <br/><a href="http://www.microsoft.com/msdownload/platformsdk/sdkupdate/">
+ * http://www.microsoft.com/msdownload/platformsdk/sdkupdate/</a>
+ *
+ * @author  knoxg
+ * 
+ */
+public class HiResTimer
+{
+    
+    
+ 
+    /** Logger instance for this class */
+    private static Logger logger = Logger.getLogger(HiResTimer.class);
+    
+    /** Set to true if the HiResTimer DLL can be found in the java library path */
+    private static boolean foundLibrary = false;
+
+    /** A HiResTimer instance; used since I want to be able to provide these methods
+     *  as static, which I found difficult in JNI directly. 
+     */
+    private static HiResTimer instance = new HiResTimer();
+
+
+    private native boolean native_isHighResTimerAvailable();
+
+    /**
+     * JNI method to return the frequency of the system clock (ticks per second)
+     *
+     * @return the frequency of the system clock (ticks per second)
+     */
+    private native long native_getFrequency();
+
+    /**
+     * JNI method to return the current timestamp (in frequency units)
+     *
+     * @return the current timestamp (in frequency units)
+     */
+    private native long native_getTimestamp();
+
+    /**
+     * Return the frequency of the available timer.
+     *
+     * @return The granularity of the timer, measured in ticks per second.
+     */
+    public static long getFrequency()
+    {
+        if (foundLibrary) {
+            return instance.native_getFrequency();
+        } else {
+            // System ostensibly supplies us with 1ms precision (although it doesn't, really)
+            return 1000;
+        }
+        
+    }
+
+    /**
+     * Returns the current value of the timer, as measured in the frequency supplied
+     * by {@link #getFrequency()}. The starting time for returned values is undefined
+     * (i.e. there is no fixed point for timestamp = 0); this value can only be used
+     * for relative timing only, or must be correlated with System.currentTimeMillis().
+     *
+     * @return The current value of the timer.
+     */
+    public static long getTimestamp()
+    {
+        if (foundLibrary) {
+            return instance.native_getTimestamp();
+        } else {
+            return System.currentTimeMillis();
+        }
+    }
+
+    /** Returns true if we can use a natively-supplied timer, false otherwise
+     *
+     * @return true if we can use a natively-supplied timer, false otherwise
+     */
+    public static boolean isNativeTimerAvailable()
+    {
+        return foundLibrary;
+    }
+
+    /**
+     * Returns true if the Windows HAL (Hardware Abstraction Layer) supports
+     * a high-resolution performance counter. Note that even if this returns false,
+     * we can fall back to a native counter, which is still more precise than
+     * the one that Java supplies.
+     *
+     * @return True if high resolution performance is available, false if not.
+     */
+    public static boolean isHiResTimerAvailable()
+    {
+        // NB: short-cut boolean logic
+        return foundLibrary && instance.native_isHighResTimerAvailable();
+    }
+
+    /** Returns the elapsed time between two timestamps, as a integer number of
+     *  milliseconds.
+     *
+     * @return the elapsed time between two timestamps, as a integer number of
+     *  milliseconds.
+     */
+    public static long getElapsedMillis(long timestamp1, long timestamp2)
+    {
+        return ((timestamp2 - timestamp1) * 1000) / getFrequency();
+    }
+
+    /** Returns the elapsed time between two timestamps, as a integer number of
+     *  microseconds.
+     *
+     * @return the elapsed time between two timestamps, as a integer number of
+     *  microseconds.
+     */
+    public static long getElapsedNanos(long timestamp1, long timestamp2)
+    {
+        return ((timestamp2 - timestamp1) * 1000000) / getFrequency();
+    }
+
+    /**
+     * Test program.
+     *
+     * @param args Command-line options
+     *
+     * @throws InterruptedException if the Thread.sleep() call was interrupted
+     */
+    public static void main(String[] args)
+        throws InterruptedException
+    {
+        System.out.println("Native timer: " +
+            (HiResTimer.isNativeTimerAvailable() ? "available" : "unavailable"));
+        System.out.println("Hi-res timer: " +
+            (HiResTimer.isHiResTimerAvailable() ? "available" : "unavailable"));
+
+        java.text.NumberFormat nf = new java.text.DecimalFormat("0.#########");
+
+        long freq = HiResTimer.getFrequency();
+
+        System.out.println("Timer has frequency of approx " + freq + " ticks/sec");
+
+        // this isn't really the granularity, you know. It's just the *maximum* precision 
+        // we can ever expect to get out of the timer. Whether the timer actually reaches
+        // this theoretical maximum is a completely different matter.
+        System.out.println("Timer has granularity of approx " +
+            nf.format(1 / (double)freq) + " seconds.");
+
+        long dStart = HiResTimer.getTimestamp();
+
+        Thread.sleep(1000);
+
+        long dEnd = HiResTimer.getTimestamp();
+
+        System.out.println("Thread.sleep() test=" +
+            HiResTimer.getElapsedMillis(dStart, dEnd) +
+            " milliseconds (should be around 1000); = " +
+            HiResTimer.getElapsedNanos(dStart, dEnd) + " microseconds");
+
+        dStart = HiResTimer.getTimestamp();
+        Thread.sleep(2000);
+        dEnd = HiResTimer.getTimestamp();
+        System.out.println("Thread.sleep() test=" +
+            HiResTimer.getElapsedMillis(dStart, dEnd) +
+            " milliseconds (should be around 2000); = " +
+            HiResTimer.getElapsedNanos(dStart, dEnd) + " microseconds");
+    }
+
+    static
+    {
+        try
+        {
+            // use the java.library.path variable first
+            System.loadLibrary("HiResTimer");
+            foundLibrary = true;
+        }
+        catch (UnsatisfiedLinkError ule)
+        {
+            // exception intentionally ignored
+        }
+
+        if (!foundLibrary)
+        {
+            // when run in WSAD, workspace.root is something like
+            //   C:/Documents and Settings/knoxg/My Documents/IBM/wsappdev51/workspace/Servers/QueryBuilderSvr.wsc
+            // ... so we can try looking in here
+            String workspaceRoot = System.getProperty("workspace.root");
+            
+            // NB, in eclipse wtp.deploy is set to something like
+            //   C:\Documents and Settings\knoxg\workspace-3.5sr2\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps
+            // so could try something relative to that as well
+
+            if (workspaceRoot != null)
+            {
+                String location = workspaceRoot +
+                    "/../../common/ext/bootstrap/HiResTimer.dll";
+
+                try {
+                    System.load(location);
+                    foundLibrary = true;
+                } catch (UnsatisfiedLinkError ule) {
+                    logger.debug(
+                        "Could not load HiResTimer from java.library.path or '" +
+                        location + "'");
+
+                    // ule.printStackTrace();
+                    // NB: we still can't use a DLL even if it's loaded in another ClassLoader
+                    // exception intentionally ignored
+                }
+            }
+        }
+
+        if (foundLibrary) {
+            HiResTimer timer = new HiResTimer();
+            logger.info("Native timer library available: high resolution timer " +
+                (timer.native_isHighResTimerAvailable() ? "available" : "unavailable"));
+        } else {
+        	logger.info("Native timer library unavailable");
+        }
+    }
+}
