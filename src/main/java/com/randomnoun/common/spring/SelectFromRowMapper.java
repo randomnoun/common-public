877406a6b298f74fca5558f5d61f1b8a51c647b2
knoxg <knoxg@192.168.0.88>
2019-05-08 11:58:06 +1000

    JdbcTemplateWithArguments, SelectFromResultSetExtractor, SelectFromRowMapper

---
+++ src/main/java/com/randomnoun/common/spring/SelectFromRowMapper.java
@@ -0,0 +1,19 @@
+package com.randomnoun.common.spring;
+
+import org.springframework.jdbc.core.RowMapper;
+
+/** A SelectFromRowMapper is pretty much the same as a RowMapper, except 
+ * it knows what columns and tables to query (i.e. the 'SELECT' and 'FROM' clauses of the SQL), 
+ * so the caller only needs to supply WHERE and ORDER BY SQL clauses
+ * 
+ * @author knoxg
+ */
+public interface SelectFromRowMapper<T> extends RowMapper<T> {
+
+	public String getSelect();
+	
+	public String getFrom();
+	
+	// this might be a good place to add pagination constraints as well, once we add that to the API
+	
+}
