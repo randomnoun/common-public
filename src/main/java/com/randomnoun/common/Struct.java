b2a0fc2a201154f95509cf863d3d35db35ea4718
knoxg <knoxg@192.168.0.88>
2019-05-08 11:51:12 +1000

    handle int[] in structuredMapToString()

--- src/main/java/com/randomnoun/common/Struct.java
+++ src/main/java/com/randomnoun/common/Struct.java
@@ -1412,6 +1412,13 @@ public class Struct {
             		for (int j=0; j<daSrc.length; j++) { daTgt[j]=daSrc[j]; }
             		List arrayList = Arrays.asList((Object[])daTgt);
             		s = s + Text.indent("  ", structuredListToString(key, arrayList));
+            	} else if (value instanceof int[]) {
+            		int[] daSrc = (int[]) value;
+            		Integer[] daTgt = new Integer[daSrc.length];
+            		for (int j=0; j<daSrc.length; j++) { daTgt[j]=daSrc[j]; }
+            		List arrayList = Arrays.asList((Object[])daTgt);
+            		s = s + Text.indent("  ", structuredListToString(key, arrayList));
+
             	} else {
             		throw new UnsupportedOperationException("Cannot convert primitive array to String");
             	}
@@ -1528,6 +1535,12 @@ public class Struct {
 	           		// @TODO convert directly probably
 	           		double[] daSrc = (double[]) value;
 	           		Double[] daTgt = new Double[daSrc.length];
+	           		for (int j=0; j<daSrc.length; j++) { daTgt[j]=daSrc[j]; }
+	           		List arrayList = Arrays.asList((Object[])daTgt);
+	           		structuredListToJson(s, (List) arrayList, jsonFormat);
+           	  	} else if (value instanceof int[]) {
+	           		int[] daSrc = (int[]) value;
+	           		Integer[] daTgt = new Integer[daSrc.length];
 	           		for (int j=0; j<daSrc.length; j++) { daTgt[j]=daSrc[j]; }
 	           		List arrayList = Arrays.asList((Object[])daTgt);
 	           		structuredListToJson(s, (List) arrayList, jsonFormat);
@@ -1649,6 +1662,17 @@ public class Struct {
             	  // @TODO convert directly probably
            		  double[] daSrc = (double[]) value;
            		  Double[] daTgt = new Double[daSrc.length];
+           		  for (int j=0; j<daSrc.length; j++) { daTgt[j]=daSrc[j]; }
+           		  List arrayList = Arrays.asList((Object[])daTgt);
+           		  if (!isFirst) { sb.append(","); }
+           		  sb.append(keyJson);
+           		  sb.append(": ");
+           		  structuredListToJson(sb, (List)arrayList, jsonFormat);
+           	  } else if (value instanceof int[]) {
+              	  // @TODO other primitive array types
+            	  // @TODO convert directly probably
+           		  int[] daSrc = (int[]) value;
+           		  Integer[] daTgt = new Integer[daSrc.length];
            		  for (int j=0; j<daSrc.length; j++) { daTgt[j]=daSrc[j]; }
            		  List arrayList = Arrays.asList((Object[])daTgt);
            		  if (!isFirst) { sb.append(","); }
